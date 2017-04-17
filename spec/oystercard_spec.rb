5ea730d8b6f125e6b2cf5b05ba2ae8e22340cd13
JayWebDevCom <jaiye.s@gmail.com>
2017-04-17 14:38:47 +0100

    Add step 5: top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,23 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
+  describe 'Initialization' do
+
+      it 'has default balance of 0' do
+        expect(oystercard.balance).to eq 0
+      end
+
+  end
+
+  describe 'Top Up' do
+
+    it 'changes balance' do
+      oystercard.top_up(20)
+      expect(oystercard.balance).to eq 20
+    end
+
+  end
+
 end
