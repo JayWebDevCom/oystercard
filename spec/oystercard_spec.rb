a7e11edcb753ab7ed292b7dcbc7cd4f175eed9c0
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-04-17 15:17:27 +0100

    step 6: maximum balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,16 +7,22 @@ describe Oystercard do
   describe 'Initialization' do
 
       it 'has default balance of 0' do
-        expect(oystercard.balance).to eq 0
+        expect(oystercard.balance).to eq described_class::DEFAULT_BALANCE
       end
 
   end
+
 
   describe 'Top Up' do
 
     it 'changes balance' do
       oystercard.top_up(20)
       expect(oystercard.balance).to eq 20
+    end
+
+    it 'can not exceed balance limit' do
+      oystercard.top_up(50)
+      expect { oystercard.top_up(41)}.to raise_error 'can not exceed limit'
     end
 
   end
